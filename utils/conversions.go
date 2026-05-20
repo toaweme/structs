dd5c9e01e1449e6a0e6390fdf5b51099284083d1
Ignas Bernotas <ignas@iber.lt>
2026-05-20 18:28:44 +0300

    fix: handle numeric types in ToInt and ToFloat

--- utils/conversions.go
+++ utils/conversions.go
@@ -11,12 +11,34 @@ import (
 
 func ToFloat(value any) (float64, error) {
 	switch v := value.(type) {
-	case float32, float64:
-		return value.(float64), nil
+	case float64:
+		return v, nil
+	case float32:
+		return float64(v), nil
+	case int:
+		return float64(v), nil
+	case int8:
+		return float64(v), nil
+	case int16:
+		return float64(v), nil
+	case int32:
+		return float64(v), nil
+	case int64:
+		return float64(v), nil
+	case uint:
+		return float64(v), nil
+	case uint8:
+		return float64(v), nil
+	case uint16:
+		return float64(v), nil
+	case uint32:
+		return float64(v), nil
+	case uint64:
+		return float64(v), nil
 	case string:
 		float, err := strconv.ParseFloat(v, 64)
 		if err != nil {
-			return 0, fmt.Errorf("failed to parse float64 default value: %s: %w", v, err)
+			return 0, fmt.Errorf("failed to parse float64 value: %s: %w", v, err)
 		}
 		return float, nil
 	default:
@@ -38,7 +60,35 @@ func ParseBool(val string) bool {
 func ToInt(value any) (int, error) {
 	switch v := value.(type) {
 	case int:
-		return value.(int), nil
+		return v, nil
+	case int8:
+		return int(v), nil
+	case int16:
+		return int(v), nil
+	case int32:
+		return int(v), nil
+	case int64:
+		return int(v), nil
+	case uint:
+		return int(v), nil
+	case uint8:
+		return int(v), nil
+	case uint16:
+		return int(v), nil
+	case uint32:
+		return int(v), nil
+	case uint64:
+		return int(v), nil
+	case float32:
+		if float32(int(v)) != v {
+			return 0, fmt.Errorf("failed to parse int from float32 with fractional part: %v", v)
+		}
+		return int(v), nil
+	case float64:
+		if float64(int(v)) != v {
+			return 0, fmt.Errorf("failed to parse int from float64 with fractional part: %v", v)
+		}
+		return int(v), nil
 	case string:
 		integer, err := strconv.Atoi(v)
 		if err != nil {
